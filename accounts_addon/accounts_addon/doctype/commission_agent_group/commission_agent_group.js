40bd7d2705203d253410423ae317141aef7b636d
Mohtashim-1m <shoaibmohtashim973@gmail.com>
2024-07-19 13:13:33 +0000

    first commit

---
+++ accounts_addon/accounts_addon/doctype/commission_agent_group/commission_agent_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Mohtashim and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Commission Agent Group', {
+	// refresh: function(frm) {
+
+	// }
+});
